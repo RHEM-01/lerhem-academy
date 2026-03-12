1875df98a32ffd29f4105641cd5842ab50d4ea9e
RHEM-01 <rhema.lemua@gmail.com>
2026-03-12 11:31:58 +0100

    Initialised the project

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { GithubIcon } from "lucide-react";
+
+export default function Login() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle className="text-xl">Welcome back!</CardTitle>
+                <CardDescription>Login with your Github Email Account</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Button className="w-full" variant="outline">
+                    <GithubIcon className="size-4" />
+                    Sign in with Github
+                </Button>
+            </CardContent>
+        </Card>
+    )
+}
